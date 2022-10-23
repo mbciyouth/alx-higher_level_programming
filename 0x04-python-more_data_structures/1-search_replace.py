bc694d72713f242a2866706cd442470d317bb061
Sunday <sundaynetspend@gmail.com>
2022-10-23 15:25:15 -0700

    task1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    new_list = my_list[:]
-    for i in range(len(new_list)):
-        if new_list[i] == search:
-            new_list[i] = replace
-            return new_list
+    return [x if x != search else replace for x in my_list]
