90e60548649d3af03283d032028f3b0e7812fc8b
Sunday <sundaynetspend@gmail.com>
2022-10-31 20:23:07 -0700

    new commit

---
+++ 0x0C-python-almost_a_circle/test/test_models/test_base.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# test_base.py
+# Sunday Adigun
+"""Defines unittests for base.py.
+
+Unittest classes:
+    TestBase_instantiation - line 23
+     TestBase_to_json_string - line 110
